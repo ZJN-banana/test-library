da07e45bf87f54fecd16fccce99f7ac8720d9875
kiki <zhangjn@yunjiglobal.com>
2020-03-05 11:31:36 +0800

    feat：测试

---
+++ precache-manifest.7915a4c8246da6950f7b468db6e9006c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50d2cf2566827a2c723204a569320745",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0fff76c33eb11fc07dc5",
+    "url": "./static/css/main.4ab7c602.chunk.css"
+  },
+  {
+    "revision": "ae5b0231d88463036d0d",
+    "url": "./static/js/2.17dd74ac.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.17dd74ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0fff76c33eb11fc07dc5",
+    "url": "./static/js/main.b1b1870d.chunk.js"
+  },
+  {
+    "revision": "1b6d6fff8fea972465b4",
+    "url": "./static/js/runtime-main.fcdbd9a7.js"
+  }
+]);
